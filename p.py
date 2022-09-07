bdbb7315f3d491d9f2403ba4e21d4a5f6c7c83b3
arthasa <82319027+arthasa28@users.noreply.github.com>
2022-09-07 10:45:36 +0700

    Add files via upload

--- p.py
+++ p.py
@@ -87,7 +87,48 @@ for i, item in enumerate(items):
 print("")
 print("Done!")
 print("")
-print("DVR cctv Telah Selasai Di Buat")
+print("
+ssitiw1104 : Se tau aku nama nya bukan putra
+ssitiw1104 :Tapi ini siapa yah?
+ssitiw1104 : Engga looooo
+ssitiw1104 : Itu mah Iqbal tmn rumah aku
+ssitiw1104 : Ngobrol aja blum pernah
+pirda273 : Bukann
+pirda273 : Salh Ketik
+ssitiw1104 : Ha?
+pirda273 : it doesn't matter
+ssitiw1104 :bSya tanya ini siapa?
+pirda273 : Saya Adalah Kamu
+pirda273 : Kamu Adalah Aku
+ssitiw1104 : Ini Siapa?
+ssitiw1104 : Kamu Menyukai aku?
+ssitiw1104 : Atau gimana sih Ndak ngerti 🙂
+pirda273 : Saya adalah orang di balik masalah ini
+ssitiw1104 : Makin ga paham dengan apa yang anda bicarakan
+pirda273 : Are you oke
+pirda273 : Kau suka sama bahits.
+pirda273 : Bahits tkj b
+ssitiw1104 : Engga
+ssitiw1104 : Udah benci ajah
+pirda273 : Apa suka sama Artha, kamu sering nempel sama dia
+
+Delay Telpon Instagram
+
+pirda273 : Apakah kamu menelpon saya, saat jam pelajaran
+pirda273 : ?
+ssitiw1104 : Ko knl tmn" sya si
+ssitiw1104 : Ha?
+ssitiw1104 : Itu tmn ku tadi
+
+pirda273 : Kamu pas ke apartemen si yang bule, kamu pas balik itu nempel mulu, kamu itu Ingin peluk.
+pirda273 : Pas kamu di boncengin arta
+ssitiw1104 : Hemh suka sii tapi ga Yaqin sya bisa milikin dia 😊😃
+ssitiw1104 : Lu siapa dh?
+ssitiw1104 : Ehh ko tau siii
+pirda273 : Karena saya sering mematai temen kau Arta bahits Radit dan teman sekelasny
+ssitiw1104 : Ohh iyahh siapa si?
+ssitiw1104 : Jujur ae
+pirda273 : Saya adalah M.IA")
 print("")
 print("")
 
